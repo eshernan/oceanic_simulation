b56d9c258b53be97c64db59eb2ecd1b8bd4189b9
Esteban Hernandez <eshernan@gmail.com>
2021-05-12 19:46:04 -0500

    initial config to download and config

---
+++ download.sh
@@ -0,0 +1,2 @@
+curl 'ftp://ftp.ifremer.fr/ifremer/croco/CODE_ARCHIVE/croco-v1.1.tar.gz' -o $INSTALLER/croco-v1.1.tar.gz
+curl 'fftp://ftp.ifremer.fr/ifremer/croco/CODE_ARCHIVE/croco_tools-v1.1.tar.gz' -o $INSTALLER/croco_tools-v1.1.tar.gz
