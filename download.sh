3a8fe5b9c4e897d3e80600dd8a75c31bf6cc3a46
Esteban Hernández <eshernan@gmail.com>
2021-11-10 08:08:07 -0500

    fixing typo error on url

--- download.sh
+++ download.sh
@@ -1,2 +1,2 @@
 wget  https://data-croco.ifremer.fr/CODE_ARCHIVE/croco-v1.1.tar.gz
-wget 'https://data-croco.ifremer.fr/CODE_ARCHIVE/croco_tools-v1.1.tar.gz
+wget https://data-croco.ifremer.fr/CODE_ARCHIVE/croco_tools-v1.1.tar.gz
